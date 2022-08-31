10d13c98ea734c93c17f9a60acc4bff974f9d80e
Francisco Ceruti <hello@fceruti.com>
2022-08-31 10:01:49 -0400

    init project

---
+++ lib/buenavista.ex
@@ -0,0 +1,18 @@
+defmodule Buenavista do
+  @moduledoc """
+  Documentation for `Buenavista`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Buenavista.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/buenavista_test.exs
@@ -0,0 +1,8 @@
+defmodule BuenavistaTest do
+  use ExUnit.Case
+  doctest Buenavista
+
+  test "greets the world" do
+    assert Buenavista.hello() == :world
+  end
+end
