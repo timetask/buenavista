417c625a2f3915303bc11e538120afe3c7b1ea54
Francisco Ceruti <hello@fceruti.com>
2022-10-25 22:34:39 -0300

    Add description and package

--- mix.exs
+++ mix.exs
@@ -6,8 +6,26 @@ defmodule BuenaVista.MixProject do
       app: :buenavista,
       version: "0.1.0",
       elixir: "~> 1.13",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
+      package: package(),
+      description: description(),
       deps: deps()
+    ]
+  end
+
+  defp description do
+    """
+    Phoenix Components
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Francisco Ceruti"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/timetask/buenavista"}
     ]
   end
 
