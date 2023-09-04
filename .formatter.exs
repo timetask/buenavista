c0640b6aada4d712df735850cd4601286888bf01
Francisco Ceruti <hello@fceruti.com>
2023-09-04 11:04:19 -0300

    Add .formatter.exs

---
+++ .formatter.exs
@@ -0,0 +1,15 @@
+locals_without_parens = [
+  variant: 2,
+  variant: 3,
+  component: 1,
+  component: 2,
+  extra_classes: 1
+]
+
+[
+  inputs: ["mix.exs", ".formatter.exs", "{lib,test}/**/*.{ex,exs}"],
+  import_deps: [:phoenix],
+  locals_without_parens: locals_without_parens,
+  export: [locals_without_parens: locals_without_parens],
+  line_length: 120
+]
