91124856953b6903dd8b57db5fdc773dfea3918a
Francisco Ceruti <hello@fceruti.com>
2023-09-12 13:09:52 -0300

    Compile newly created modules on the fly

--- lib/mix/tasks/buenavista.sync.config.ex
+++ lib/mix/tasks/buenavista.sync.config.ex
@@ -11,6 +11,18 @@ defmodule Mix.Tasks.Buenavista.Sync.Config do
   def run(_opts) do
     for %BuenaVista.Bundle{} = bundle <- BuenaVista.Config.get_bundles() do
       BuenaVista.Generator.sync(bundle)
+
+      if match?(%BuenaVista.Bundle.Nomenclator{}, bundle.nomenclator) do
+        unless function_exported?(bundle.nomenclator.module_name, :__info__, 1) do
+          Code.compile_file(bundle.nomenclator.file)
+        end
+      end
+
+      if match?(%BuenaVista.Bundle.Hydrator{}, bundle.hydrator) do
+        unless function_exported?(bundle.hydrator.module_name, :__info__, 1) do
+          Code.compile_file(bundle.hydrator.file)
+        end
+      end
     end
   end
 end
