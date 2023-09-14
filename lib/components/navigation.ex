0b5d33197acb043957ed7e6abaac861a55fd4e20
Francisco Ceruti <hello@fceruti.com>
2023-09-14 09:23:56 -0300

    Add navigation component

--- lib/components/navigation.ex
+++ lib/components/navigation.ex
@@ -1,3 +1,34 @@
 defmodule BuenaVista.Components.Navigation do
   use BuenaVista.Component
+
+  defmodule NavItem do
+    use TypedStruct
+
+    typedstruct enforce: true do
+      field :url, String.t()
+      field :nav, atom()
+      field :item, any()
+    end
+  end
+
+  variant :orientation, [:vertical, :horizontal], :vertical
+
+  classes [:list_item_class]
+
+  attr :items, :list, default: []
+
+  slot :link do
+    attr :url, :string, required: true
+    attr :nav, :atom, values: [:navigate, :patch, :href], required: true
+  end
+
+  component navigation(assigns) do
+    ~H"""
+    <nav class={[@base_class, @variant_classes]}>
+      <.link :for={item <- @items} class={@list_item_class} {[{item.nav, item.url}]}>
+        <%= render_slot(@link, item.item) %>
+      </.link>
+    </nav>
+    """
+  end
 end
