70e0b0410fb66b0db21ed36b0496f1087c2d8ede
Francisco Ceruti <hello@fceruti.com>
2023-10-06 17:27:27 -0300

    Refactor default hydrator to use new code style

--- lib/template/default_hydrator.ex
+++ lib/template/default_hydrator.ex
@@ -47,82 +47,82 @@ defmodule BuenaVista.Template.DefaultHydrator do
   # BuenaVista.Components.Typography                             heading
   # ---------------------------------------------------------------------
 
-  style [:heading, :classes, :base_class], ~CSS"""
-    color: <%= @color_text %>;
+  style :heading, :base_class, ~CSS"""
+    color: $color_text;
     flex-direction: column;
     align-items: flex-start;
     display: flex;
   """
 
-  style [:heading, :classes, :title_tag_class], ~CSS"""
+  style :heading, :title_tag_class, ~CSS"""
     font-weight: bold;
   """
 
-  style [:heading, :classes, :inline_class], ~CSS"""
+  style :heading, :inline_class, ~CSS"""
     font-weight: normal;
     font-size: 70%;
   """
 
-  style [:heading, :classes, :actions_class], ~CSS"""
+  style :heading, :actions_class, ~CSS"""
     color: red;
   """
 
-  # style [:heading, :decoration, :accent], ~CSS"""
+  # style :heading, :decoration, :accent, ~CSS"""
   # """
-  style [:heading, :decoration, :spaced_uppcase], ~CSS"""
-    color: <%= @color_text_subtle %>;
+  style :heading, :decoration, :spaced_uppcase, ~CSS"""
+    color: $color_text_subtle;
     text-transform: uppercase;
     letter-spacing: 0.2rem;
 
-    .<%= class_name(:heading, :classes, :title_tag_class) %> {
+    .$heading__title_tag_class {
       font-weight: 300;
     }
   """
 
-  style [:heading, :size, :sm], ~CSS"""
-    font-size: <%= @font_sm %>;
+  style :heading, :size, :sm, ~CSS"""
+    font-size: $font_sm;
   """
 
-  style [:heading, :size, :md], ~CSS"""
-    font-size: <%= @font_md %>;
+  style :heading, :size, :md, ~CSS"""
+    font-size: $font_md;
   """
 
-  style [:heading, :size, :lg], ~CSS"""
-    font-size: <%= @font_lg %>;
+  style :heading, :size, :lg, ~CSS"""
+    font-size: $font_lg;
   """
 
-  style [:heading, :size, :xl], ~CSS"""
-    font-size: <%= @font_xl %>;
+  style :heading, :size, :xl, ~CSS"""
+    font-size: $font_xl;
   """
 
-  style [:heading, :size, :xxl], ~CSS"""
-    font-size: <%= @font_xxl %>;
+  style :heading, :size, :xxl, ~CSS"""
+    font-size: $font_xxl;
   """
 
   # ---------------------------------------------------------------------
   # BuenaVista.Components.Navigation                     navigation_item
   # ---------------------------------------------------------------------
 
-  # style [:navigation_item, :classes, :base_class], ~CSS"""
+  # style :navigation_item, :base_class, ~CSS"""
   # """
 
-  # style [:navigation_item, :state, :selected], ~CSS"""
+  # style :navigation_item, :state, :selected, ~CSS"""
   # """
 
   # ---------------------------------------------------------------------
   # BuenaVista.Components.Navigation                          navigation
   # ---------------------------------------------------------------------
 
-  style [:navigation, :classes, :base_class], ~CSS"""
+  style :navigation, :base_class, ~CSS"""
     display: flex;
   """
 
-  style [:navigation, :orientation, :vertical], ~CSS"""
+  style :navigation, :orientation, :vertical, ~CSS"""
     flex-direction: column;
   """
 
-  style [:navigation, :orientation, :horizontal], ~CSS"""
-    gap: <%= @gap_sm %>;
+  style :navigation, :orientation, :horizontal, ~CSS"""
+    gap: $gap_sm;
     flex-direction: row;
   """
 
@@ -130,83 +130,83 @@ defmodule BuenaVista.Template.DefaultHydrator do
   # BuenaVista.Components.Modal                                   drawer
   # ---------------------------------------------------------------------
 
-  # style [:drawer, :classes, :base_class], ~CSS"""
+  # style :drawer, :base_class, ~CSS"""
   # """
-  # style [:drawer, :classes, :modal_content_class], ~CSS"""
+  # style :drawer, :modal_content_class, ~CSS"""
   # """
-  # style [:drawer, :classes, :close_btn_class], ~CSS"""
-  # """
-
-  # style [:drawer, :position, :top], ~CSS"""
-  # """
-  # style [:drawer, :position, :right], ~CSS"""
-  # """
-  # style [:drawer, :position, :bottom], ~CSS"""
-  # """
-  # style [:drawer, :position, :left], ~CSS"""
+  # style :drawer, :close_btn_class, ~CSS"""
   # """
 
-  # style [:drawer, :size, :sm], ~CSS"""
+  # style :drawer, :position, :top, ~CSS"""
   # """
-  # style [:drawer, :size, :md], ~CSS"""
+  # style :drawer, :position, :right, ~CSS"""
   # """
-  # style [:drawer, :size, :lg], ~CSS"""
+  # style :drawer, :position, :bottom, ~CSS"""
   # """
-  # style [:drawer, :size, :full], ~CSS"""
+  # style :drawer, :position, :left, ~CSS"""
+  # """
+
+  # style :drawer, :size, :sm, ~CSS"""
+  # """
+  # style :drawer, :size, :md, ~CSS"""
+  # """
+  # style :drawer, :size, :lg, ~CSS"""
+  # """
+  # style :drawer, :size, :full, ~CSS"""
   # """
 
   # ---------------------------------------------------------------------
   # BuenaVista.Components.Modal                                    modal
   # ---------------------------------------------------------------------
 
-  # style [:modal, :classes, :base_class], ~CSS"""
+  # style :modal, :base_class, ~CSS"""
   # """
-  # style [:modal, :classes, :content_class], ~CSS"""
+  # style :modal, :content_class, ~CSS"""
   # """
-  # style [:modal, :classes, :close_class], ~CSS"""
-  # """
-
-  # style [:modal, :position, :centered], ~CSS"""
-  # """
-  # style [:modal, :position, :top], ~CSS"""
+  # style :modal, :close_class, ~CSS"""
   # """
 
-  # style [:modal, :size, :sm], ~CSS"""
+  # style :modal, :position, :centered, ~CSS"""
   # """
-  # style [:modal, :size, :md], ~CSS"""
+  # style :modal, :position, :top, ~CSS"""
   # """
-  # style [:modal, :size, :lg], ~CSS"""
+
+  # style :modal, :size, :sm, ~CSS"""
   # """
-  # style [:modal, :size, :full], ~CSS"""
+  # style :modal, :size, :md, ~CSS"""
+  # """
+  # style :modal, :size, :lg, ~CSS"""
+  # """
+  # style :modal, :size, :full, ~CSS"""
   # """
 
   # ---------------------------------------------------------------------
   # BuenaVista.Components.Layout                     multi_column_layout
   # ---------------------------------------------------------------------
 
-  # style [:multi_column_layout, :classes, :base_class], ~CSS"""
+  # style :multi_column_layout, :base_class, ~CSS"""
   # """
 
   # ---------------------------------------------------------------------
   # BuenaVista.Components.Layout                          sidebar_layout
   # ---------------------------------------------------------------------
 
-  style [:sidebar_layout, :classes, :base_class], ~CSS"""
+  style :sidebar_layout, :base_class, ~CSS"""
     display: flex;
   """
 
-  style [:sidebar_layout, :classes, :sidebar_class], ~CSS"""
-    width: <%= @sidebar_width %>;
+  style :sidebar_layout, :sidebar_class, ~CSS"""
+    width: $sidebar_width;
     flex-grow: 1;
   """
 
-  style [:sidebar_layout, :classes, :main_class], ~CSS"""
+  style :sidebar_layout, :main_class, ~CSS"""
     min-inline-size: 50%;
     flex-grow: 999;
     flex-basis: 0;
   """
 
-  style [:sidebar_layout, :position, :right], ~CSS"""
+  style :sidebar_layout, :position, :right, ~CSS"""
     flex-direction: row-reversed;
   """
 
@@ -214,111 +214,111 @@ defmodule BuenaVista.Template.DefaultHydrator do
   # BuenaVista.Components.Input                                    input
   # ---------------------------------------------------------------------
 
-  # style [:input, :classes, :base_class], ~CSS"""
+  # style :input, :base_class, ~CSS"""
   # """
 
-  # style [:input, :type, :text], ~CSS"""
+  # style :input, :type, :text, ~CSS"""
   # """
-  # style [:input, :type, :number], ~CSS"""
+  # style :input, :type, :number, ~CSS"""
   # """
-  # style [:input, :type, :email], ~CSS"""
+  # style :input, :type, :email, ~CSS"""
   # """
-  # style [:input, :type, :password], ~CSS"""
+  # style :input, :type, :password, ~CSS"""
   # """
-  # style [:input, :type, :radio], ~CSS"""
+  # style :input, :type, :radio, ~CSS"""
   # """
-  # style [:input, :type, :checkbox], ~CSS"""
+  # style :input, :type, :checkbox, ~CSS"""
   # """
-  # style [:input, :type, :hidden], ~CSS"""
+  # style :input, :type, :hidden, ~CSS"""
   # """
 
   # ---------------------------------------------------------------------
   # BuenaVista.Components.Input                              input_group
   # ---------------------------------------------------------------------
 
-  # style [:input_group, :classes, :base_class], ~CSS"""
+  # style :input_group, :base_class, ~CSS"""
   # """
 
-  # style [:input_group, :layout, :vertical], ~CSS"""
+  # style :input_group, :layout, :vertical, ~CSS"""
   # """
-  # style [:input_group, :layout, :table], ~CSS"""
+  # style :input_group, :layout, :table, ~CSS"""
   # """
-  # style [:input_group, :layout, :stacked], ~CSS"""
+  # style :input_group, :layout, :stacked, ~CSS"""
   # """
 
-  # style [:input_group, :state, :success], ~CSS"""
+  # style :input_group, :state, :success, ~CSS"""
   # """
-  # style [:input_group, :state, :danger], ~CSS"""
+  # style :input_group, :state, :danger, ~CSS"""
   # """
 
   # ---------------------------------------------------------------------
   # BuenaVista.Components.Input                                    label
   # ---------------------------------------------------------------------
 
-  # style [:label, :classes, :base_class], ~CSS"""
+  # style :label, :base_class, ~CSS"""
   # """
 
-  # style [:label, :state, :success], ~CSS"""
+  # style :label, :state, :success, ~CSS"""
   # """
-  # style [:label, :state, :danger], ~CSS"""
+  # style :label, :state, :danger, ~CSS"""
   # """
 
   # ---------------------------------------------------------------------
   # BuenaVista.Components.Form                                      form
   # ---------------------------------------------------------------------
 
-  # style [:form, :classes, :base_class], ~CSS"""
+  # style :form, :base_class, ~CSS"""
   # """
 
-  # style [:form, :display, :block], ~CSS"""
+  # style :form, :display, :block, ~CSS"""
   # """
-  # style [:form, :display, :inline], ~CSS"""
+  # style :form, :display, :inline, ~CSS"""
   # """
 
   # ---------------------------------------------------------------------
   # BuenaVista.Components.Button                                  button
   # ---------------------------------------------------------------------
 
-  # style [:button, :classes, :base_class], ~CSS"""
+  # style :button, :base_class, ~CSS"""
   # """
 
-  # style [:button, :size, :xs], ~CSS"""
+  # style :button, :size, :xs, ~CSS"""
   # """
-  # style [:button, :size, :sm], ~CSS"""
+  # style :button, :size, :sm, ~CSS"""
   # """
-  # style [:button, :size, :md], ~CSS"""
+  # style :button, :size, :md, ~CSS"""
   # """
-  # style [:button, :size, :lg], ~CSS"""
-  # """
-
-  # style [:button, :color, :nav], ~CSS"""
-  # """
-  # style [:button, :color, :ctrl], ~CSS"""
-  # """
-  # style [:button, :color, :primary], ~CSS"""
-  # """
-  # style [:button, :color, :success], ~CSS"""
-  # """
-  # style [:button, :color, :danger], ~CSS"""
-  # """
-  # style [:button, :color, :warning], ~CSS"""
-  # """
-  # style [:button, :color, :info], ~CSS"""
+  # style :button, :size, :lg, ~CSS"""
   # """
 
-  # style [:button, :style, :filled], ~CSS"""
+  # style :button, :color, :nav, ~CSS"""
   # """
-  # style [:button, :style, :outline], ~CSS"""
+  # style :button, :color, :ctrl, ~CSS"""
   # """
-  # style [:button, :style, :soft], ~CSS"""
+  # style :button, :color, :primary, ~CSS"""
   # """
-  # style [:button, :style, :link], ~CSS"""
+  # style :button, :color, :success, ~CSS"""
   # """
-  # style [:button, :style, :transparent], ~CSS"""
+  # style :button, :color, :danger, ~CSS"""
+  # """
+  # style :button, :color, :warning, ~CSS"""
+  # """
+  # style :button, :color, :info, ~CSS"""
   # """
 
-  # style [:button, :border, :thin], ~CSS"""
+  # style :button, :style, :filled, ~CSS"""
   # """
-  # style [:button, :border, :thick], ~CSS"""
+  # style :button, :style, :outline, ~CSS"""
+  # """
+  # style :button, :style, :soft, ~CSS"""
+  # """
+  # style :button, :style, :link, ~CSS"""
+  # """
+  # style :button, :style, :transparent, ~CSS"""
+  # """
+
+  # style :button, :border, :thin, ~CSS"""
+  # """
+  # style :button, :border, :thick, ~CSS"""
   # """
 end
