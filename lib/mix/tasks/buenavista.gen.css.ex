f7572e2623ea5be11652b42a51c73da6f4528a23
Francisco Ceruti <hello@fceruti.com>
2023-10-11 18:19:10 -0300

    Renamed tasks

--- lib/mix/tasks/buenavista.gen.css.ex
+++ lib/mix/tasks/buenavista.gen.css.ex
@@ -1,18 +1,16 @@
-defmodule Mix.Tasks.Bv.Gen.Css do
+defmodule Mix.Tasks.Buenavista.Gen.Css do
   use Mix.Task
   require Logger
 
   @requirements ["app.config"]
-  @component_apps Application.compile_env(:buenavista, :apps)
 
   @shortdoc "Generates CSS files (use `--help` for options)"
   def run(opts) do
     {parsed_opts, _, _} = OptionParser.parse(opts, strict: [theme: :keep])
 
     themes = BuenaVista.Themes.get_themes() |> filter_by_name(parsed_opts)
-    apps = @component_apps
 
-    BuenaVista.Generator.generate_css_files(themes, apps)
+    BuenaVista.Generator.generate_css_files(themes)
   end
 
   defp filter_by_name(themes, parsed_opts) do
---
+++ lib/mix/tasks/buenavista.gen.themes.ex
@@ -0,0 +1,51 @@
+defmodule Mix.Tasks.Buenavista.Gen.Themes do
+  use Mix.Task
+  require Logger
+
+  @requirements ["app.config"]
+
+  @shortdoc "(Re)Generates Nomenclator and Hydrator modules"
+  def run(opts) do
+    {parsed_opts, _, _} = OptionParser.parse(opts, strict: [core: :boolean])
+
+    themes = get_themes(parsed_opts)
+
+    BuenaVista.Generator.generate_theme_files(themes)
+  end
+
+  defp get_themes(parsed_opts) do
+    if Keyword.get(parsed_opts, :core, false),
+      do: get_core_themes(),
+      else: BuenaVista.Themes.get_themes() |> filter_by_name(parsed_opts)
+  end
+
+  defp filter_by_name(themes, parsed_opts) do
+    theme_names = Keyword.get_values(parsed_opts, :theme)
+
+    if Enum.empty?(theme_names),
+      do: themes,
+      else: Enum.filter(themes, fn theme -> theme.name in theme_names end)
+  end
+
+  defp get_core_themes() do
+    config = [
+      apps: [
+        [name: :buenavista_galeria]
+      ],
+      config: [
+        base_module: BuenaVista.Themes,
+        extend: :hydrator,
+        themes_dir: "lib/themes",
+        hydrator_imports: [
+          BuenaVista.Constants.DefaultColors,
+          BuenaVista.Constants.DefaultSizes
+        ]
+      ],
+      themes: [
+        [name: "default", output: false]
+      ]
+    ]
+
+    BuenaVista.ConfigReader.build_themes(config)
+  end
+end
